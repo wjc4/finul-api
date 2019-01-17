92148cd973abae76f8ff4c1c11d94b8f1274ac76
weijian <chen_wei_jian5566@hotmail.com>
2019-01-18 04:48:30 +0800

    handle reject

--- inu/static/js/pollotp.js
+++ inu/static/js/pollotp.js
@@ -2,8 +2,12 @@ function doPoll(){
     $.get('auth_status/'+sessionStorage.getItem("userid"), function(data) {
         console.log(data)
         if(data['state'] == 2){
-            console.log("otp confirmed")
+            console.log("otp confirmed");
             window.open('confirm_transaction/'+sessionStorage.getItem("userid"),"_self");
+        } else if(data['state'] == 0){
+            console.log("tx rejected");
+            alert('Transaction is rejected.');
+            window.open('transaction',"_self");
         }
         setTimeout(doPoll,2000);
     });
