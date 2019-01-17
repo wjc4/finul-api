271bb2c710ae6e2fa188d91a4ea51865fa0b2a61
See Jie Xun <jxsee94@yahoo.com.sg>
2019-01-18 02:04:32 +0800

    success edited

--- inu/routes/authenticate.py
+++ inu/routes/authenticate.py
@@ -20,10 +20,39 @@ def authenticate_code(user_id, code):
     if status:
         data['state'] = 2
         db.update(user_id, data)
-        return render_template('success.html')
+        trans = confirm_transaction(user_id)
+        return render_template('success.html', amount=trans['amount'], name=trans['receiver_name'], receiver_id=trans['receiver_id'], description=trans['description'])
     # return jsonify({'status': status, 'user_id': user_id})
-    
 
+def confirm_transaction(sender_id):
+    sender_data = db.get(sender_id)
+
+    pending = sender_data['pending']
+    amount = pending['amount']
+    if float(amount) > sender_data['balance']:
+        return jsonify({'error': 'Account has insufficient balance for this transaction.'})
+
+    receiver_id = pending['receiver_id']
+    receiver_data = db.get(receiver_id)
+    sender_data = confirm_pending(sender_data, sender_id)
+    receiver_data = confirm_pending(receiver_data, receiver_id)
+    db.update(sender_id, sender_data)
+    db.update(receiver_id, receiver_data)
+    return pending
+    # return jsonify({'status': True, 'transaction': pending})
+
+def confirm_pending(data, id):
+    pending = data['pending']
+    amount = pending['amount']
+    if id == pending['sender_id']:
+        data['balance'] -= float(amount)
+    elif id == pending['receiver_id']:
+        data['balance'] += float(amount)
+    data['pending'] = None
+    data['state'] = 0 # reset to 0 (no transaction pending) after confirming transfer
+    data['transactions'].append(pending)
+    return data
+    
 # @app.route('/auth_start/<user_id>', methods=['GET']) # browser will call this when a transaction is requested.
 # def start(user_id):
 #     if not db.check_exist(user_id):
