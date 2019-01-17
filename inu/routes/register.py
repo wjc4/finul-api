f487361584be38318e092ab0fc43b5e68e3b8b02
See Jie Xun <jxsee94@yahoo.com.sg>
2019-01-17 18:07:11 +0800

    minor function rename

--- inu/routes/register.py
+++ inu/routes/register.py
@@ -6,7 +6,7 @@ from inu import db
 import pyotp
 
 @app.route('/register/<user_id>', methods=['GET'])
-def evaluate():
+def register():
     random_secret = pyotp.random_base32()
     user_data = {}
     user_data['secret'] = random_secret
