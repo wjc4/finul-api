14cbca1d64325f24ebcc5bcd1337e41631b8b1a4
weijian <chen_wei_jian5566@hotmail.com>
2019-01-18 03:32:33 +0800

    fixed online auth

--- inu/static/js/pollotp.js
+++ inu/static/js/pollotp.js
@@ -2,9 +2,10 @@ function doPoll(){
     $.get('auth_status/'+sessionStorage.getItem("userid"), function(data) {
         console.log(data)
         if(data['state'] == 2){
-            window.open('confirm_transaction/'+sessionStorage.getItem("userid"))
+            console.log("otp confirmed")
+            window.open('confirm_transaction/'+sessionStorage.getItem("userid"),"_self");
         }
-        setTimeout(doPoll,5000);
+        setTimeout(doPoll,2000);
     });
 }
 
